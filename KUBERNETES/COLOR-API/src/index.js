2ae72552a2030518a51a7964d9fd3b0e94444657
nzisequrity <nzi.stephanek+github@gmail.com>
2026-02-09 20:52:41 -0500

    fixing index file

--- KUBERNETES/COLOR-API/src/index.js
+++ KUBERNETES/COLOR-API/src/index.js
@@ -3,8 +3,25 @@ const express = require('express');
 const app = express();
 const port = 80;
 
+const hostname = os.hostname();
+const color = 'blue';
+
 app.get('/', (req, res) => {
-    res.send('<h1 style="color: blue;">Hello World from color-api!</h1>');
+    res.send(`<h1 style="color: ${color};">Hello World from color-api!</h1>
+<h2>Hostname: ${hostname}</h2>`);
+});
+
+app.get('/api', (req, res) => {
+    const format = req.query.format
+
+    if (format === 'json') {
+        return res.json({ 
+            color,
+            hostname
+        });
+    } else {
+        return res.send(`Color: ${color}, Hostname: ${hostname}`);
+    }
 });
 
 app.listen(port, () => {
